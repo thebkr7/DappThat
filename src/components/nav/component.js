6ef1269f7ea40bb3313bac95acd486812d206023
Raymond Huynh <raymond.h@myplanet.com>
2018-06-09 16:38:34 -0400

    Change class to className

--- src/components/nav/component.js
+++ src/components/nav/component.js
@@ -3,33 +3,33 @@ import React, { Component } from 'react';
 class Nav extends Component {
   render() {
     return (
-      <div class="hero-head">
-        <nav class="navbar">
-          <div class="container">
-            <div class="navbar-brand">
-              <a class="navbar-item" href="../..">
+      <div className="hero-head">
+        <nav className="navbar">
+          <div className="container">
+            <div className="navbar-brand">
+              <a className="navbar-item" href="../..">
                 <img src="http://bulma.io/images/bulma-type-white.png" alt="Logo" />
               </a>
-              <span class="navbar-burger burger" data-target="navbarMenu">
+              <span className="navbar-burger burger" data-target="navbarMenu">
                 <span></span>
                 <span></span>
                 <span></span>
               </span>
             </div>
-            <div id="navbarMenu" class="navbar-menu">
-              <div class="navbar-end">
-                <span class="navbar-item">
-                  <a class="button is-white is-outlined" href="#">
-                    <span class="icon">
-                      <i class="fa fa-home"></i>
+            <div id="navbarMenu" className="navbar-menu">
+              <div className="navbar-end">
+                <span className="navbar-item">
+                  <a className="button is-white is-outlined" href="#">
+                    <span className="icon">
+                      <i className="fa fa-home"></i>
                     </span>
                     <span>DAPPS</span>
                   </a>
                 </span>
-                <span class="navbar-item">
-                  <a class="button is-white is-outlined" href="#">
-                    <span class="icon">
-                      <i class="fa fa-book"></i>
+                <span className="navbar-item">
+                  <a className="button is-white is-outlined" href="#">
+                    <span className="icon">
+                      <i className="fa fa-book"></i>
                     </span>
                     <span>About</span>
                   </a>
