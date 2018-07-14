50cbb5d99287bdd10a478b9b050522ea92819be2
Mr. Benji <thebkrcanbefound@gmail.com>
2018-07-14 17:20:08 -0400

    redesigned home page css wise

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import 'bulma/css/bulma.css';
 import './App.css';
 import BannerComponent from './banner/component';
-import ServicesComponent from './services/component';
-import AboutComponent from './about/component';
-import ContactComponent from './contact/component';
+// import ServicesComponent from './services/component';
+// import AboutComponent from './about/component';
+// import ContactComponent from './contact/component';
 
 class App extends Component {
   render() {
     return (
       <div>
         <BannerComponent />
-        <ServicesComponent />
+        {/* <ServicesComponent />
         <AboutComponent />
-        <ContactComponent />
+        <ContactComponent /> */}
       </div>
     );
   }
--- src/components/banner/component.jsx
+++ src/components/banner/component.jsx
@@ -7,39 +7,38 @@ import Norway from '../../assets/Norway.jpg';
 class Banner extends React.Component {
   render() {
     return(
-      <div className="hero">
-        <div className='background'>
-          <img src={Norway} />
-          <NavComponent />
-          <div className="hero-body banner-position">
-            <div className="container has-text-centered">
-              <div className="column is-6 is-offset-3">
-                <h1 className="title has-text-light">
-                  DappThat
-                </h1>
-                <h2 className="subtitle has-text-light">
-                  We break things, build things, and break more things. We do this on the blockchain so that it is
-                  irreversable. Use our service :)
-                </h2>
-                <div className="box">
+      <div>
 
-                  <div className="field is-grouped">
-                    <p className="control is-expanded">
-                      <input className="input" type="text" placeholder="Enter your email"/>
-                    </p>
-                    <p className="control">
-                      <a className="button is-info">
-                        Notify Me
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
+
+      <section class="hero background-blue is-fullheight centered">
+
+        <img src={Norway} className='img-size'/>
+        <NavComponent />
+
+          <div class="container see-through-me">
+          <h1 class="title is-2 hp-title has-text-centered has-text-light">
+            dAppThat
+          </h1>
+          <p class="subtitle is-5 has-text-centered has-text-light">
+            DappThat is a <strong className="has-text-light">One Stop Shop</strong> for blockchain application development. We have a skillful team of fullstack and blockchain engineers that build quality, secure, decentralized applications for a brighter tomorrow.
+          </p>
+
+
+            <div className="field is-grouped">
+              <p className="control is-expanded">
+                <input className="input" type="text" placeholder="Enter your email" />
+              </p>
+              <p className="control">
+                <a className="button is-info">
+                  Get In Touch
+                </a>
+              </p>
             </div>
-          </div>
-        </div>
-      </div>
 
+        </div>
+      </section>
+
+      </div>
     )
   }
 }
